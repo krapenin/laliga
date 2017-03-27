ea444467fdb1b0fec0f50cfbbe0c324fc563d047
Marcos Vilches <mvilches@clb.t-prisa.com>
2017-03-27 19:54:06 +0200

    Added simple list for players

--- src/AppBundle/Controller/PlayerController.php
+++ src/AppBundle/Controller/PlayerController.php
@@ -10,10 +10,20 @@ namespace AppBundle\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class PlayerController extends Controller
 {
     public function listAction() {
 
+        $em = $this->getDoctrine()->getManager();
+        $players = $em->getRepository("AppBundle:Player")->findAll();
+
+        return $this->render('players/index.html.twig', [
+            "players" => $players
+        ]);
     }
+
+
+
 }

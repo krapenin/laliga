175986ea4c2e4bb6d1868e307656ada0afdb64a9
Marcos Vilches <mvilches@clb.t-prisa.com>
2017-03-27 21:54:41 +0200

    Added fully player crud

--- src/AppBundle/Controller/PlayerController.php
+++ src/AppBundle/Controller/PlayerController.php
@@ -8,22 +8,88 @@
 
 namespace AppBundle\Controller;
 
-
+use AppBundle\Entity\Player;
+use AppBundle\Form\PlayerType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 
 class PlayerController extends Controller
 {
     public function listAction() {
 
-        $em = $this->getDoctrine()->getManager();
-        $players = $em->getRepository("AppBundle:Player")->findAll();
+        $playerManager = $this->get('player.manager');
 
         return $this->render('players/index.html.twig', [
-            "players" => $players
+            "players" => $playerManager->get()
         ]);
     }
 
+    //TODO Refactorizar handler formulario
+    public function newAction(Request $request)
+    {
+        $player = new Player();
+
+        $form = $this->createForm(PlayerType::class, $player);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $playerManager = $this->get('player.manager');
+            $playerManager->update($player);
+
+            return $this->redirect($this->generateUrl("laliga_players"));
+        }
+
+        return $this->render('players/new.html.twig', [
+            "form" => $form->createView()
+        ]);
+    }
+
+    public function editAction($id, Request $request)
+    {
 
 
+        $playerManager = $this->get('player.manager');
+        $player = $playerManager->get($id);
+
+        if (!$player) {
+            $this->addError("Player Not Found");
+            return $this->redirect($this->generateUrl("laliga_players"));
+        }
+
+        $form = $this->createForm(PlayerType::class, $player);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $playerManager = $this->get('player.manager');
+            $playerManager->update($player);
+            return $this->redirect($this->generateUrl("laliga_players"));
+        }
+
+        return $this->render('players/new.html.twig', [
+            "form" => $form->createView(),
+        ]);
+    }
+
+    public function deleteAction($id)
+    {
+        $playerManager = $this->get('player.manager');
+
+        if (!$playerManager->delete($id)) {
+            $this->addError("Couldnt delete player");
+        }
+
+        $this->addSuccess("Player deleted");
+
+        return $this->redirect($this->generateUrl("laliga_players"));
+    }
+
+    protected function addError($msg)
+    {
+        //Listener para las excepciones, enviar errores automaticamente
+        $this->get('session')->getFlashBag()->add("error", $msg);
+    }
+
+    protected function addSuccess($msg)
+    {
+        $this->get('session')->getFlashBag()->add("success", $msg);
+    }
 }
--- src/AppBundle/Form/PlayerType.php
+++ src/AppBundle/Form/PlayerType.php
@@ -9,7 +9,23 @@
 namespace AppBundle\Form;
 
 
-class PlayerType
-{
+use AppBundle\Entity\Player;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
+class PlayerType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add("name", TextType::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            "data_class" => Player::class
+        ]);
+    }
 }

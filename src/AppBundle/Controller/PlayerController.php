99b6cb258fa82149586b1bab840ef83ace10ea95
Marcos Vilches <mvilches@clb.t-prisa.com>
2017-03-27 19:31:21 +0200

    added player entity nad controller and new bundle routing

---
+++ src/AppBundle/Controller/PlayerController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mvilches
+ * Date: 27/03/17
+ * Time: 19:23
+ */
+
+namespace AppBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class PlayerController extends Controller
+{
+    public function listAction() {
+
+    }
+}
